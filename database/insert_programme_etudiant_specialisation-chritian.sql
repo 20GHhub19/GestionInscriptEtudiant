d27ffa70685c08046b9edbae30304aa5d2ded814
Christian Castaneda <kriztianz19037760@gmail.com>
2026-03-17 21:59:51 -0400

    creation insert_programme_etudiant_specialisation-chritian.sql

---
+++ database/insert_programme_etudiant_specialisation-chritian.sql
@@ -0,0 +1,26 @@
+﻿
+(0 rows affected)
+
+(10 rows affected)
+
+(6 rows affected)
+
+(20 rows affected)
+Checking identity information: current identity value '1000'.
+DBCC execution completed. If DBCC printed error messages, contact your system administrator.
+Checking identity information: current identity value '15'.
+DBCC execution completed. If DBCC printed error messages, contact your system administrator.
+Checking identity information: current identity value '122'.
+DBCC execution completed. If DBCC printed error messages, contact your system administrator.
+Checking identity information: current identity value '1'.
+DBCC execution completed. If DBCC printed error messages, contact your system administrator.
+
+(20 rows affected)
+
+(6 rows affected)
+
+(10 rows affected)
+
+(10 rows affected)
+
+Completion time: 2026-03-15T21:34:17.9515861-04:00
